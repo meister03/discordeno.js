fc66aa0a6572a281898113dd72b7c23c00bc6319
meister03 <root@_HOSTNAME_>
2022-05-05 11:52:28 +0200

    Add toggles to base level for destruct object

--- Structures/DestructObject.js
+++ Structures/DestructObject.js
@@ -8,6 +8,12 @@ class DestructObject {
     for (let [key, value] of Object.entries(message)) {
       if (!removeFields[key]) {
         this[key] = value;
+        // Add toggles to base level
+        if(key === 'toggles') {
+          for (let [toggleKey, toggleValue] of Object.entries(this[key].list())) {
+            this[toggleKey] = toggleValue;
+          }
+        }
       } else {
         this[`_${key}`] = value;
       }
